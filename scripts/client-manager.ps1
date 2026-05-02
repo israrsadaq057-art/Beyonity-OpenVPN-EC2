476b1bc952a7c9c9833e561a116f51d7e82f7ddb
Israr Sadaq <israrsadaq057@gmail.com>
2026-05-02 09:37:08 +0200

    Initial commit: Beyonity OpenVPN Server on EC2
    INFRASTRUCTURE:
    - EC2 t3.micro for OpenVPN server
    - Security group with UDP 1194
    - Client certificate management
    - Artist remote access solution
    
    SCRIPTS:
    - deploy-openvpn.ps1 - Complete EC2 setup
    - client-manager.ps1 - Add/revoke artists
    
    CLIENTS:
    - anna, ben, carla, david .ovpn files
    
    DOCUMENTATION:
    - Complete README with architecture
    - Deployment instructions
    - Verification commands
    
    IT Administrator: Israr Sadaq | CCNA | CCNP

---
+++ scripts/client-manager.ps1
@@ -0,0 +1,27 @@
+﻿# BEYONITY OPENVPN CLIENT MANAGEMENT
+# Add or revoke artist certificates
+
+param(
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("add", "revoke", "list")]
+    [string]$Action,
+    
+    [string]$ClientName
+)
+
+$ServerIp = "13.218.143.190"
+$KeyPath = "C:\beyonity-cicd-project\ConsoleRenderKey.pem"
+
+if ($Action -eq "add") {
+    Write-Host "Adding client: $ClientName" -ForegroundColor Yellow
+    ssh -i $KeyPath ubuntu@$ServerIp "sudo bash openvpn-install.sh client add $ClientName"
+    Write-Host "Download config: scp -i $KeyPath ubuntu@$ServerIp:/home/ubuntu/$ClientName.ovpn ." -ForegroundColor Gray
+}
+elseif ($Action -eq "revoke") {
+    Write-Host "Revoking client: $ClientName" -ForegroundColor Yellow
+    ssh -i $KeyPath ubuntu@$ServerIp "sudo bash openvpn-install.sh client revoke $ClientName"
+}
+elseif ($Action -eq "list") {
+    Write-Host "Listing all clients" -ForegroundColor Yellow
+    ssh -i $KeyPath ubuntu@$ServerIp "sudo bash openvpn-install.sh client list"
+}
